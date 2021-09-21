9716610cef93a4e6a89241693a9f87618d7b7c24
Molly Singmaster <mollysingmaster@gmail.com>
2021-09-20 21:59:57 -0400

    added comments to script.js to clarify code and describe its functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,15 @@
 // GIVEN a weather dashboard with form inputs
-// WHEN I search for a city
-// THEN I am presented with current and future conditions for that city and that city is added to the search history
-// WHEN I view current weather conditions for that city
-// THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
-// WHEN I view the UV index
-// THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
-// WHEN I view future weather conditions for that city
-// THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
+//✅WHEN I search for a city
+//✅THEN I am presented with current and future conditions for that city and that city is added to the search history
+//✅WHEN I view current weather conditions for that city
+//✅THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
+//✅WHEN I view the UV index
+//✅THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
+//✅WHEN I view future weather conditions for that city
+//✅THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
 // WHEN I click on a city in the search history
 // THEN I am again presented with current and future conditions for that city
+
 var searchBtnEl = $("#save-button");
 var tempTextEl = $("#temp");
 var windTextEl = $("#wind");
@@ -17,6 +18,7 @@ var uvTextEl = $("#uv");
 var cityTextEl = $("#city");
 var inputField = $(input);
 
+// gets the city name from the user and displays it along with the current time. It also informs our weather further down
 function getApi() {
   var city = $(inputField).val();
   var requestUrl =
@@ -28,6 +30,7 @@ function getApi() {
     .then(function (response) {
       return response.json();
     })
+    // defined the temp, wind, and humidity. Then it adds that to the p tags I hardcoded with labels
     .then(function (data) {
       console.log(data);
       var tempMain = data.main.temp;
@@ -41,6 +44,7 @@ function getApi() {
       var lat = data.coord.lat;
       var lon = data.coord.lon;
 
+    // api link to get the latitude and longitude of the area, needed for the uv index
       var requestUrlUv =
         "http://api.openweathermap.org/data/2.5/onecall?&units=imperial&lat=" +
         lat +
@@ -51,6 +55,7 @@ function getApi() {
         .then(function (response) {
           return response.json();
         })
+        // displays the uv index and color codes it depending on the number
         .then(function (data) {
           console.log(data);
           var uvColor = data.current.uvi;
@@ -71,23 +76,27 @@ function getApi() {
             uvTextEl.attr("class", "uv-violet");
           }
 
+        //   gets the 5 day forecast for the city that the user searched for
           var forecastInfo = '';
           forecastInfo += city;
           $.each(data.daily, function (index, val) {
+            //   loops through the different days and creates a list of values to display like wind, humidity, and temperature
             if (index < 5) {
-                forecastInfo += "<p>";
-                forecastInfo += "<b>Day: " + (index + 1) + "</b><br>";
-                forecastInfo += "<b>Temp: " + val.temp.day + " &degF</b><br>";
-                forecastInfo += "<b>Wind: " + val.wind_speed + " MPH</b><br>";
-                forecastInfo += "<b>Humidity: " + val.humidity + " %</b><br>";
+                forecastInfo += "<ul>";
+                forecastInfo += "<li><b>Day: " + (index + 1) + "</b></li>";
+                forecastInfo += "<li><b>Temp: " + val.temp.day + " &degF</b><br></li>";
+                forecastInfo += "<li><b>Wind: " + val.wind_speed + " MPH</b><br></li>";
+                forecastInfo += "<li><b>Humidity: " + val.humidity + " %</b></li>";
                 forecastInfo += "<img src='https://openweathermap.org/img/wn/" + val.weather[0].icon + "@2x.png'>";
                 forecastInfo += val.weather[0].description;
-                forecastInfo += "</p>";
+                forecastInfo += "</ul>";
             }
           })
+        //   displays the text content of the unordered list above and not just what is in the quotes
           $("#forecast").html(forecastInfo);
         });
     });
 }
 
+// when the search button is clicked, the getApi function is called
 searchBtnEl.on("click", getApi);
