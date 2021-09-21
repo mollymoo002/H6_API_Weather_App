94495fcccdb97e681a80387684b587573a61ca17
Molly Singmaster <mollysingmaster@gmail.com>
2021-09-20 21:44:20 -0400

    added logic to get the city's temp, wind, humidity, and uv index. Also added logic to get the 5 day forecast and loop through each day

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,20 +9,85 @@
 // THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
 // WHEN I click on a city in the search history
 // THEN I am again presented with current and future conditions for that city
-var searchBtnEl = document.getElementById("save-button");
-var tempTextEl = document.getElementById("temp");
-var windTextEl = document.getElementById("wind");
-var humidTextEl = document.getElementById("humidity");
-var uvTextEl = document.getElementById("uv");
-var cityTextEl = document.getElementById("city");
+var searchBtnEl = $("#save-button");
+var tempTextEl = $("#temp");
+var windTextEl = $("#wind");
+var humidTextEl = $("#humidity");
+var uvTextEl = $("#uv");
+var cityTextEl = $("#city");
+var inputField = $(input);
 
-var requestUrl = "http://api.openweathermap.org/data/2.5/weather?q=philadelphia&appid=7ea56fe80e9e577a7387e69996a6f935";
-console.log(requestUrl);
-fetch(requestUrl) 
+function getApi() {
+  var city = $(inputField).val();
+  var requestUrl =
+    "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=7ea56fe80e9e577a7387e69996a6f935";
+  var today = moment().format("MM/DD/YYYY");
+  var cityName = cityTextEl;
+  cityName.text(city + ", " + today);
+  fetch(requestUrl)
     .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
-        console.log(data);
-      });
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      var tempMain = data.main.temp;
+      var windMain = data.wind.speed;
+      var humidityMain = data.main.humidity;
 
+      tempTextEl.text(tempMain);
+      humidTextEl.text(humidityMain);
+      windTextEl.text(windMain + "MPH");
+
+      var lat = data.coord.lat;
+      var lon = data.coord.lon;
+
+      var requestUrlUv =
+        "http://api.openweathermap.org/data/2.5/onecall?&units=imperial&lat=" +
+        lat +
+        "&lon=" +
+        lon +
+        "&appid=7ea56fe80e9e577a7387e69996a6f935";
+      fetch(requestUrlUv)
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (data) {
+          console.log(data);
+          var uvColor = data.current.uvi;
+          uvTextEl.text(uvColor);
+          if (uvColor <= 2) {
+            uvTextEl.attr("class", "uv-green");
+          }
+          if (uvColor >= 3 && uvColor <= 5) {
+            uvTextEl.attr("class", "uv-yellow");
+          }
+          if (uvColor >= 6 && uvColor <= 7) {
+            uvTextEl.attr("class", "uv-orange");
+          }
+          if (uvColor >= 8 && uvColor <= 10) {
+            uvTextEl.attr("class", "uv-red");
+          }
+          if (uvColor >= 10) {
+            uvTextEl.attr("class", "uv-violet");
+          }
+
+          var forecastInfo = '';
+          forecastInfo += city;
+          $.each(data.daily, function (index, val) {
+            if (index < 5) {
+                forecastInfo += "<p>";
+                forecastInfo += "<b>Day: " + (index + 1) + "</b><br>";
+                forecastInfo += "<b>Temp: " + val.temp.day + " &degF</b><br>";
+                forecastInfo += "<b>Wind: " + val.wind_speed + " MPH</b><br>";
+                forecastInfo += "<b>Humidity: " + val.humidity + " %</b><br>";
+                forecastInfo += "<img src='https://openweathermap.org/img/wn/" + val.weather[0].icon + "@2x.png'>";
+                forecastInfo += val.weather[0].description;
+                forecastInfo += "</p>";
+            }
+          })
+          $("#forecast").html(forecastInfo);
+        });
+    });
+}
+
+searchBtnEl.on("click", getApi);
