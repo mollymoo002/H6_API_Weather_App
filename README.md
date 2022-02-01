07bff9cbd4a9bd1208d62dc4fdeba645d37df87d
Molly Singmaster <mollysingmaster@gmail.com>
2022-01-31 20:21:12 -0500

    added the screenshot of the app in the readme

--- README.md
+++ README.md
@@ -1,13 +1,11 @@
 # H6_API_Weather_App
 ## Description
 The weather application allows the user to search for a city and get information not only about the current weather patterns, but also the next 5 days of weather forecasts. This is useful for people who are looking ahead if they are making plans or they know an event like a wedding is coming up. This was a true test of api skill and I learned a lot about using third party APIs to take their information and display it on something I created.
-## Installation
-What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.
 ## Usage
 Please search for a city and press the search button. At the top, you see the current weather and if you scroll further down, you will see the next 5 days weather forecasts.
 
 
-   [screenshot of weather app](assets/images/screenshot.jpg)
+   ![screenshot of weather app](assets/images/screenshot.jpg)
 
 ## Credits
 - Ask BCS
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,3 @@
-// GIVEN a weather dashboard with form inputs
-//✅WHEN I search for a city
-//✅THEN I am presented with current and future conditions for that city and that city is added to the search history
-//✅WHEN I view current weather conditions for that city
-//✅THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
-//✅WHEN I view the UV index
-//✅THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
-//✅WHEN I view future weather conditions for that city
-//✅THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
-// WHEN I click on a city in the search history
-// THEN I am again presented with current and future conditions for that city
-
 var searchBtnEl = $("#save-button");
 var tempTextEl = $("#temp");
 var windTextEl = $("#wind");
@@ -31,6 +19,7 @@ function getApi() {
     .then(function (response) {
       return response.json();
     })
+
     // defined the temp, wind, and humidity. Then it adds that to the p tags I hardcoded with labels
     .then(function (data) {
       var tempMain = data.main.temp;
@@ -48,6 +37,7 @@ function getApi() {
             var buttonList = searchHistory.html('<button id="history-button">' + cityHistory[i] + '</button>');
             buttonList++;
       };
+
       getCityHistory();
       cityHistory.push(city);
       localStorage.setItem("City", JSON.stringify(cityHistory));
@@ -66,6 +56,7 @@ function getApi() {
         .then(function (response) {
           return response.json();
         })
+
         // displays the uv index and color codes it depending on the number
         .then(function (data) {
           var uvColor = data.current.uvi;
@@ -88,7 +79,6 @@ function getApi() {
 
         //   gets the 5 day forecast for the city that the user searched for
           var forecastInfo = '';
-          forecastInfo += city;
           $.each(data.daily, function (index, val) {
             //   loops through the different days and creates a list of values to display like wind, humidity, and temperature
             if (index < 5) {
@@ -102,6 +92,7 @@ function getApi() {
                 forecastInfo += "</ul>";
             }
           })
+
         //   displays the text content of the unordered list above and not just what is in the quotes
           $("#forecast").html(forecastInfo);
         });
